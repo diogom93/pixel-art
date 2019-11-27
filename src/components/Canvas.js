ad9562d37869166b776a2b5c6251b0fd854de7e6
Diogo Martins <nunodiogomartins@gmail.com>
2019-11-27 00:33:14 +0000

    Save current canvas
    Save current canvas to localStorage and resume it on reload.

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -3,15 +3,22 @@ import Pixel from './Pixel';
 import Colours from '../Colours';
 
 const Canvas = props => {
-    const [matrix, setMatrix] = useState(Array(30)
+    const savedCanvas = localStorage.getItem('savedCanvas');
+    const startingCanvas = savedCanvas ? JSON.parse(savedCanvas) : Array(30)
         .fill()
         .map(() => Array(30)
-            .fill(0)));
+            .fill(0));
+    const [matrix, setMatrix] = useState(startingCanvas);
 
     const changeColour = (rowIndex, colIndex) => {
         const matrixAux = JSON.parse(JSON.stringify(matrix));
         matrixAux[rowIndex][colIndex] = props.currentColour;
         setMatrix(matrixAux);
+        saveCurrentCanvas(matrixAux);
+    }
+
+    const saveCurrentCanvas = matrixAux => {
+        localStorage.setItem('savedCanvas', JSON.stringify(matrixAux));
     }
 
     return (
